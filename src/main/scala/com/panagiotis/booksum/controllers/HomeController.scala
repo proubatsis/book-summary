048d3ec7d2e24c924405d4b1832b6be3994cec49
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-04-22 21:06:21 -0400

    Mustache template sample

--- src/main/scala/com/panagiotis/booksum/controllers/HomeController.scala
+++ src/main/scala/com/panagiotis/booksum/controllers/HomeController.scala
@@ -2,12 +2,16 @@ package com.panagiotis.booksum.controllers
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.response.Mustache
+
+@Mustache("home")
+case class HomeView(A: String, B: String)
 
 /**
   * Created by panagiotis on 22/04/17.
   */
 class HomeController extends Controller {
-  get("/") { _:Request =>
-    "Hello, world!"
+  get("/") { req: Request =>
+    HomeView("Test Title", "desc... lol :)")
   }
 }
