1384d8bf37a6867091b76cff09cb60d775b4fad9
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-05-09 06:56:00 -0400

    Added image field to database

--- initialize-db.sql
+++ initialize-db.sql
@@ -3,6 +3,7 @@ CREATE TABLE book (
 	isbn TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
 	author TEXT NOT NULL,
+	image TEXT,
 	description TEXT
 );
 
@@ -13,8 +14,8 @@ CREATE TABLE book_summary (
 );
 
 INSERT INTO book (isbn, title, author, description) VALUES
-	('1234', 'Harry Potter', 'JK Rowling', 'Magic stuff!'),
-	('5678', 'Sword of Truth', 'Terry Goodkind', 'Richard - Seeker of Truth');
+	('1234', 'Harry Potter', 'JK Rowling', 'https://i.jeded.com/i/harry-potter-and-the-goblet-of-fire.11171.jpg', 'Magic stuff!'),
+	('5678', 'Sword of Truth', 'Terry Goodkind', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/The_Sword_of_Truth_cover.jpg/220px-The_Sword_of_Truth_cover.jpg', 'Richard - Seeker of Truth');
 
 
 INSERT INTO book_summary (book_id, summary) VALUES
--- src/main/scala/ca/panagiotis/booksum/models/Book.scala
+++ src/main/scala/ca/panagiotis/booksum/models/Book.scala
@@ -4,4 +4,4 @@ package ca.panagiotis.booksum.models
   * Created by panagiotis on 22/04/17.
   */
 
-case class Book(id: Int, isbn: String,  title: String, author: String, description: Option[String])
+case class Book(id: Int, isbn: String,  title: String, author: String, description: Option[String], image: Option[String])
