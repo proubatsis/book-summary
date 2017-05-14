8bbacddbff8f6470f8a44c3d8d29a8a5e4e81146
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-05-14 13:34:21 -0400

    Better routing

--- src/main/scala/ca/panagiotis/booksum/controllers/BookController.scala
+++ src/main/scala/ca/panagiotis/booksum/controllers/BookController.scala
@@ -11,33 +11,25 @@ import com.twitter.finatra.http.Controller
   * Created by panagiotis on 23/04/17.
   */
 class BookController @Inject() (bookService: BookService, bookDataService: BookDataService) extends Controller {
-  get("/books/summary") { request: BookGetRequest =>
-    request.id match {
-      case Some(id) =>
-        for {
-          result <- bookService.findBookSummary(id)
-        } yield {
-          result match {
-            case Some((b, s)) => BookSummaryView(b.title, b.author, b.description, s)
-            case None => response.notFound(NotFoundView(s"Book: ${request.id}"))
-          }
-        }
-      case None => response.badRequest()
+  get("/books/:id/summary") { request: BookGetRequest =>
+    for {
+      result <- bookService.findBookSummary(request.id.head)
+    } yield {
+      result match {
+        case Some((b, s)) => BookSummaryView(b.title, b.author, b.description, s)
+        case None => response.notFound(NotFoundView(s"Book: ${request.id}"))
+      }
     }
   }
 
-  get("/books/description") { request: BookGetRequest =>
-    request.externalId match {
-      case Some(externalId) =>
-        for {
-          result <- bookDataService.getBook(externalId)
-        } yield {
-          result match {
-            case Some(bookData) => BookDescriptionView.fromBookData(bookData)
-            case None => response.notFound(NotFoundView(s"Book: ${request.externalId}"))
-          }
-        }
-      case None => response.badRequest()
+  get("/books/ext/:external_id/description") { request: BookGetRequest =>
+    for {
+      result <- bookDataService.getBook(request.externalId.head)
+    } yield {
+      result match {
+        case Some(bookData) => BookDescriptionView.fromBookData(bookData)
+        case None => response.notFound(NotFoundView(s"Book: ${request.externalId}"))
+      }
     }
   }
 
--- src/main/scala/ca/panagiotis/booksum/controllers/requests/BookSearchRequest.scala
+++ src/main/scala/ca/panagiotis/booksum/controllers/requests/BookSearchRequest.scala
@@ -1,12 +1,12 @@
 package ca.panagiotis.booksum.controllers.requests
 
 import com.twitter.finagle.http.Request
-import com.twitter.finatra.request.RouteParam
+import com.twitter.finatra.request.QueryParam
 
 /**
   * Created by panagiotis on 13/05/17.
   */
 case class BookSearchRequest(
                             request: Request,
-                            @RouteParam q: Option[String]
+                            @QueryParam q: Option[String]
                             )
