377dbb0454f07497ef2f69d0458a0647a9e4c774
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-05-26 07:14:14 -0400

    Setup environment variables to override configuration

--- src/main/scala/ca/panagiotis/booksum/modules/QuillModule.scala
+++ src/main/scala/ca/panagiotis/booksum/modules/QuillModule.scala
@@ -2,13 +2,25 @@ package ca.panagiotis.booksum.modules
 
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
+import com.typesafe.config.ConfigValueFactory
+import io.getquill.util.LoadConfig
 import io.getquill.{FinaglePostgresContext, SnakeCase}
 
 /**
   * Created by panagiotis on 22/04/17.
   */
 object QuillModule extends TwitterModule {
-  lazy val fpctx = new FinaglePostgresContext[SnakeCase]("ctx")
+  private val baseConfig = LoadConfig("ctx")
+  private val dbHost = sys.env.getOrElse("BOOKSUM_DB_HOST", baseConfig.getString("host"))
+  private val dbUser = sys.env.getOrElse("BOOKSUM_DB_USER", baseConfig.getString("user"))
+  private val dbPassword = sys.env.getOrElse("BOOKSUM_DB_PASSWORD", baseConfig.getString("password"))
+  lazy val fpctx = new FinaglePostgresContext[SnakeCase](
+    baseConfig
+      .withValue("host", ConfigValueFactory.fromAnyRef(dbHost))
+      .withValue("user", ConfigValueFactory.fromAnyRef(dbUser))
+      .withValue("password", ConfigValueFactory.fromAnyRef(dbPassword))
+  )
+
 
   @Singleton
   @Provides
