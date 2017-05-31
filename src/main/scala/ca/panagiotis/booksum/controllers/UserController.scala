072a28760850fedacf60e13b9d8c4cfd99d4063d
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-05-30 22:56:43 -0400

    Added logout

--- src/main/scala/ca/panagiotis/booksum/controllers/UserController.scala
+++ src/main/scala/ca/panagiotis/booksum/controllers/UserController.scala
@@ -4,11 +4,12 @@ import java.lang.Exception
 import javax.inject.Inject
 
 import ca.panagiotis.booksum.controllers.requests.{AccountRequest, CreateUserRequest, LoginRequest}
+import ca.panagiotis.booksum.filters.AuthorizeFilter
 import ca.panagiotis.booksum.models.BooksumUser
 import ca.panagiotis.booksum.services.{BookService, UserService}
 import ca.panagiotis.booksum.util.{Endpoint, Token}
 import ca.panagiotis.booksum.views._
-import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.{Request, Status}
 import com.twitter.finatra.http.Controller
 import com.twitter.util.Future
 import ca.panagiotis.booksum.filters.contexts.AccountContext._
@@ -18,9 +19,15 @@ import ca.panagiotis.booksum.filters.contexts.AccountContext._
   * Created by panagiotis on 21/05/17.
   */
 class UserController @Inject() (userService: UserService, bookService: BookService) extends Controller {
+  private val ACCESS_TOKEN_COOKIE = "access"
+
   get("/login") { req: Request =>
     val to = req.params.getOrElse("to", "/")
     LoginView(None, None, None, to)
+  }
+
+  get("/logout") { _: Request =>
+    response.status(Status.SeeOther).location("/login").cookie(ACCESS_TOKEN_COOKIE, "")
   }
 
   get("/signup") { req: Request =>
@@ -52,7 +59,7 @@ class UserController @Inject() (userService: UserService, bookService: BookServi
     } yield userAccount match {
       case Some((user, account)) => {
         if (BooksumUser.isValidPassword(user, req.password))
-          response.status(303).location(req.to).cookie("access", Token.encodeAccessToken(account))
+          response.status(Status.SeeOther).location(req.to).cookie(ACCESS_TOKEN_COOKIE, Token.encodeAccessToken(account))
         else invalidLoginView
       }
       case None => invalidLoginView
