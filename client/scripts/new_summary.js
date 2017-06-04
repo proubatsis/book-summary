f71f4ae0fc3f39bc0b2f7531c6ef13f72bb2aa71
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-06-04 12:04:32 -0400

    Added default message

--- client/scripts/new_summary.js
+++ client/scripts/new_summary.js
@@ -5,13 +5,29 @@ var $ = require("jquery");
 var EDITOR = "#summaryEditor";
 var PREVIEW = "#summaryPreview";
 
+var DEFAULT_MESSAGE = "# Start typing\n\n";
+DEFAULT_MESSAGE += "This is where your summary preview will be displayed. Formatting is done via **markdown**.\n\n";
+DEFAULT_MESSAGE += "You do not need to use markdown, but if you would like to add formatting to your summary then it will be helpful to learn the basics."
+DEFAULT_MESSAGE += "For example, you can make lists, or use bold and italics.\n\n";
+DEFAULT_MESSAGE += "- Plot point 1\n";
+DEFAULT_MESSAGE += "- **Plot** point 2\n";
+DEFAULT_MESSAGE += "- *Plot* point 3\n";
+DEFAULT_MESSAGE += "## How the above message was formatted\n\n";
+DEFAULT_MESSAGE += "\`# Start typing\`\n\n";
+DEFAULT_MESSAGE += "\`This is where your summary preview will be displayed. Formatting is done via **markdown**.\`\n\n";
+DEFAULT_MESSAGE += "\`You do not need to use markdown, but if you would like to add formatting to your summary then it will be helpful to learn the basics.\`\n\n"
+DEFAULT_MESSAGE += "\`For example, you can make lists, or use bold and italics.\`\n\n";
+DEFAULT_MESSAGE += "\`- Plot point 1\`\n\n";
+DEFAULT_MESSAGE += "\`- **Plot** point 2\`\n\n";
+DEFAULT_MESSAGE += "\`- *Plot* point 3\`\n\n";
+
 // Update preview
 var NewSummViewModel = function() {
     this.summaryArea = ko.observable();
 
     this.rendered = ko.computed(function() {
         var summary = this.summaryArea();
-        return markdown.toHTML(summary ? summary : "");
+        return markdown.toHTML(summary ? summary : DEFAULT_MESSAGE);
     }, this);
 };
 
